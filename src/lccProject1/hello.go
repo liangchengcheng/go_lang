893a3a03123ecdb3106c49eb4816eb71e7f0da98
liangchengcheng <1038127753@qq.com>
2018-03-23 08:54:21 +0800

    add simple go file
    add simple go file

---
+++ src/lccProject1/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var a1 = "11"
+var b1 string = "runoob.com"
+var c1 bool
+
+func main()  {
+	fmt.Println("Hello world!")
+	var a = 1.5
+	var b =2
+	fmt.Println(a,b)
+
+	println(a1, b1, c1)
+}
